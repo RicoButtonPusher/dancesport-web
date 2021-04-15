d112a10fe951ba84353aee1be3c4eb55a3f9c454
Parker Lynn <parkerdlynn@gmail.com>
2021-04-14 19:13:28 -0700

    Resetting Article.js

--- src/pages/News.js
+++ src/pages/News.js
@@ -32,7 +32,7 @@ import latestNews from '../components/backend-data/latestNews';
         <div className="news--article-container">
           <div className="news-article">
             <Link to={"/" + id} className="news-article-title">
-              {title}
+              {title} updating
             </Link>
             <div className="news-article-text"> {text} </div>
             <div className="news-article-info">
